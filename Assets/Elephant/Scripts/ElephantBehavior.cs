dc7e1bce15c6032df96861c401e42e998ddbae1f
44378 <chen.jiayua@husky.neu.ledu>
2020-07-22 12:26:09 -0400

    elephant

---
+++ Assets/Elephant/Scripts/ElephantBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElephantBehavior : MonoBehaviour
+{
+    public Transform waterSprayPoint;
+
+    public GameObject waterParticle;
+
+    float sprayCountDown = 0.1f;
+    float timer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetButton("Fire1")) {
+            if(timer >= sprayCountDown) {
+                GameObject water = Instantiate(waterParticle, waterSprayPoint.position, waterParticle.transform.rotation) as GameObject;
+                Destroy(water, 3f);
+                timer = 0f;
+            }
+        }
+        timer += Time.deltaTime;
+        
+    }
+}
---
+++ Assets/Elephant/Scripts/ParticleTriggers.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTriggers : MonoBehaviour
+{
+    public ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        int i = 0;
+
+        while (i < numCollisionEvents)
+        {
+            if (other.tag == "Fire")
+            {
+                other.SetActive(false);
+            }
+            i++;
+        }
+    }
+}
