8c109ef6360b5f5869ff9112b0f38f47f461ec7e
Michael Bi <michaelbi777@gmail.com>
2020-07-29 02:56:54 -0400

    added enemy AI, adjusted camera

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
+    public Vector3 relativePlacement = new Vector3(0, 0.3f, -1);
     public Transform focus;
-    public float minDistance = 10;
-    public float rangeDistance = 5;
-    public float minVertical = 10;
-    public float verticalRange = 60;
+    public float minVertical = -60;
+    public float maxVertical = 20;
     public float senseHorizontal = 1;
     public float senseVertical = 0.5f;
     public static float horizontal = 0;
     public static float vertical = 0;
-    public static float distance = 0;
+    
 
     Vector3 relativePos;
 
@@ -25,34 +24,36 @@ public class PlayerCamera : MonoBehaviour
         focus = GameObject.FindGameObjectWithTag("Player").transform;
         horizontal = 0;
         vertical = 0;
-        relativePos = (0.2f * rangeDistance + minDistance) * Vector3.back; 
+        relativePos = relativePlacement; 
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {   
         float lookVertical = Input.GetAxis("Mouse Y");
 
-        vertical += lookVertical * senseVertical;
+        vertical -= lookVertical * senseVertical;
         if (vertical < minVertical) { vertical = minVertical; }
-        if (vertical > minVertical + verticalRange) { vertical = minVertical + verticalRange; }
-
-        distance = (vertical - minVertical) / verticalRange * rangeDistance + minDistance;       
+        if (vertical > maxVertical) { vertical = maxVertical; }
+       
         
         float lookHorizontal = Input.GetAxis("Mouse X");
         horizontal = (horizontal + lookHorizontal * senseHorizontal) % 360;
 
         //Debug.Log(lookHorizontal + " " + horizontal + " " + lookVertical + " " + vertical);
         
-        Quaternion rotation = Quaternion.Euler(vertical, horizontal, 0);
+        Quaternion rotation = Quaternion.Euler(0, horizontal, 0);
 
-        relativePos = rotation * Vector3.back * distance;  
+        relativePos = rotation * relativePlacement; 
         
         Vector3 playerPos = focus.transform.position;
         this.transform.position = playerPos + relativePos;
 
         transform.LookAt(focus);
 
-        Cursor.lockState = CursorLockMode.Locked;
+        Vector3 currentRota = transform.rotation.eulerAngles;
+
+        transform.rotation = Quaternion.Euler(vertical, currentRota.y, currentRota.z);
 
     }
 }
