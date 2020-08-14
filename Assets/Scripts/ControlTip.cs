214ef7f41521d949cf98f78b5867cec72ee06584
Michael Bi <michaelbi777@gmail.com>
2020-08-13 20:16:34 -0400

    easily portable parts of inspect/control tip

---
+++ Assets/Scripts/ControlTip.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlTip : MonoBehaviour
+{
+    public string message;
+    public string destroyKey = "None";
+    public float duration = -1;
+    public int range = -1;
+    GameObject[] chars;
+    ControlTipBox ctb;
+    float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+        chars = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehavior>().chars;
+        ctb = GameObject.FindGameObjectWithTag("TipBox").GetComponent<ControlTipBox>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (duration != -1)
+        {
+            time += Time.deltaTime;
+            if (time > duration)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        if (range == -1 || Vector3.Distance(chars[PlayerBehavior.activeChar].transform.position, transform.position) < range)
+        {
+            ctb.SetText(Vector3.Distance(chars[PlayerBehavior.activeChar].transform.position, transform.position), message);
+        }
+    }
+}
---
+++ Assets/Scripts/ControlTipBox.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlTipBox : MonoBehaviour
+{
+    public float fadeTime = 1;
+    float bestRange;
+    Text tipBox;
+
+    float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tipBox = gameObject.GetComponent<Text>();
+        bestRange = 10000;
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > fadeTime)
+        {
+            tipBox.enabled = false;
+            bestRange = 10000;
+        }
+        else
+        {
+            time += Time.deltaTime;
+            bestRange += Time.deltaTime;
+        }
+        
+    }
+
+    public void SetText(float range, string message)
+    {
+        if(range < bestRange)
+        {
+            tipBox.enabled = true;
+            bestRange = range;
+            tipBox.text = message;
+        }
+    }
+}
