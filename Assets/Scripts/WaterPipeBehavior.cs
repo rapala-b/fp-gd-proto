4c0cb4c3f709362401723c82639ff68116be92b7
Maddy Van Hulse <44590929+madelinehulse@users.noreply.github.com>
2020-08-14 00:08:32 -0500

    additions to basement

---
+++ Assets/Scripts/WaterPipeBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPipeBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Check if collider is elephant
+        if (collision.gameObject.CompareTag("Pet1"))
+        {
+            GameObject water = GameObject.FindGameObjectWithTag("WaterSource");
+            water.SetActive(true);
+        }
+    }
+}
