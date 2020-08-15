105be72d4586784cf372c639ae28c9dd7d8c3557
Maddy Van Hulse <44590929+madelinehulse@users.noreply.github.com>
2020-08-14 19:00:49 -0500

    add candy tracking

---
+++ Assets/Scripts/StatsMenuBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsMenuBehavior : MonoBehaviour
+{
+
+    public TMPro.TMP_Text candyOne;
+    public TMPro.TMP_Text candyTwo;
+    public TMPro.TMP_Text candyThree;
+    public TMPro.TMP_Text candyTotal;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        candyOne.text = "Level 1 Candy: " + PlayerPrefs.GetInt("levelOneCandyFound").ToString() + "/"
+           + PlayerPrefs.GetInt("levelOneCandy").ToString();
+        candyTwo.text = "Level 2 Candy: " + PlayerPrefs.GetInt("levelTwoCandyFound").ToString() + "/"
+           + PlayerPrefs.GetInt("levelTwoCandy").ToString();
+        candyThree.text = "Level 3 Candy: " + PlayerPrefs.GetInt("levelThreeCandyFound").ToString() + "/"
+           + PlayerPrefs.GetInt("levelThreeCandy").ToString();
+        candyTotal.text = "Total Candy: " + PlayerPrefs.GetInt("levelOneCandyFound").ToString() + "/"
+           + (PlayerPrefs.GetInt("levelOneCandy") + PlayerPrefs.GetInt("levelTwoCandy") + PlayerPrefs.GetInt("levelThreeCandy")).ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
